258ae34e74fd2ba91698cc7046c273371ce19a84
martyn <m_g_lewis@hotmail.com>
2023-12-08 20:54:58 +0000

    Whatever

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ pip3 install -r requirements.txt
 ```
 pip3 freeze > requirements.txt
 ```
+
--- build.sh
+++ build.sh
@@ -11,6 +11,10 @@ function check_success() {
     fi
 }
 
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
+
 if [[ -z "$VIRTUAL_ENV" ]]; then
   source venv/bin/activate
 fi
