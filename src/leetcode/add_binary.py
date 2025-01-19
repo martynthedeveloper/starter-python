bec0d7739068471bd442207675e7521106ed7532
Martyn <m_g_lewis@hotmail.com>
2025-01-19 15:56:18 +0000

    add_binary with copilot

--- src/leetcode/add_binary.py
+++ src/leetcode/add_binary.py
@@ -1,3 +1,33 @@
+# https://leetcode.com/problems/add-binary/description/
+
+
 def add_binary(a: str, b: str) -> str:
-    answer = str(int(a) + int(b))
+
+    size = max(len(a), len(b))
+    answer = ""
+    carry = 0
+
+    for i in range(size):
+
+        aa = 0 if i >= len(a) else int(a[len(a) - i - 1])
+        bb = 0 if i >= len(b) else int(b[len(b) - i - 1])
+
+        ia = aa + bb + carry
+
+        if ia == 0:
+            answer = "0" + answer
+            carry = 0
+        elif ia == 1:
+            answer = "1" + answer
+            carry = 0
+        elif ia == 2:
+            answer = "0" + answer
+            carry = 1
+        else:  # ia == 3
+            answer = "1" + answer
+            carry = 1
+
+    if carry:
+        answer = "1" + answer
+
     return answer
--- test/leetcode/test_add_binary.py
+++ test/leetcode/test_add_binary.py
@@ -9,3 +9,23 @@ def test_example_1():
 def test_example_2():
     answer = add_binary("1010", "1011")
     assert answer == "10101"
+
+
+def test_example_3():
+    answer = add_binary("1010", "0101")
+    assert answer == "1111"
+
+
+def test_example_4():
+    answer = add_binary("1010", "0100")
+    assert answer == "1110"
+
+
+def test_example_5():
+    answer = add_binary("11", "")
+    assert answer == "11"
+
+
+def test_example_6():
+    answer = add_binary("1111", "1111")
+    assert answer == "11110"
