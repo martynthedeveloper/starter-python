d72f2e69428a5bdbf2877d73cfd3647db27fd0ca
Martyn <m_g_lewis@hotmail.com>
2025-01-19 17:11:17 +0000

    my_sqrt

--- src/leetcode/my_sqrt.py
+++ src/leetcode/my_sqrt.py
@@ -1,12 +1,32 @@
 def my_sqrt(x: int) -> int:
-    if x == 1:
-        return 1
+    if x == 0:
+        return 0
 
-    answer = 0
-    for i in range(x):
-        if i * i <= x:
-            answer = i
+    left = 0
+    right = int(x / 2)
+    guess = 0
+
+    while right - left > 1:
+
+        guess = left + int((right - left) / 2)
+        square = guess * guess
+
+        if square == x:
+            return guess
+
+        if square < x:
+            left = guess
         else:
-            break
+            right = guess
 
-    return answer
+    return guess
+
+
+# . sqrt(8)
+# . 0 1 2 3 4
+# . l   g   r s=4
+# .     l g r s=9
+# .     l r   s=
+# .
+# .
+# .
