6f0be827a97ec54af387bd41d19df964c5409621
Martyn <m_g_lewis@hotmail.com>
2025-03-02 15:56:58 +0000

    win11

--- source_me.sh
+++ source_me.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Run this with source source_me.sh
-
-source venv/bin/activate
+if test -f "venv/Scripts/activate"; then
+	source venv/scripts/activate
+else
+        source venv/bin/activate
+fi
 
 echo "Python Virtual environment activated."
